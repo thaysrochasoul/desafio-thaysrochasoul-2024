3c20af5a55378d076db46b7bc3d5deb1f13da307
Thays Rocha <130604997+thaysrochasoul@users.noreply.github.com>
2024-09-11 10:16:22 -0300

    Add files via upload

---
+++ recintos-zoo.js
@@ -0,0 +1,25 @@
+class RecintosZoo {
+
+
+
+
+    analisaRecintos(animal, quantidade) {
+    
+
+        const animais = [{especie: "LEAO", tamanho: 3, bioma: "Savana"},
+                         {especie: "MACACO", tamanho: 1, bioma: "Savana", "Floresta"},
+                         {especie: "LEOPARDO", tamanho: 2, bioma: "Savana"},
+                         {especie: "CROCODILO", tamanhho: 3, bioma: "Rio"},
+                         {especie:"GAZELA", tamanho: 2, bioma: "Savana"},
+                         {especie: "HIPOPOTAMO", tamanho: 4, bioma: "Savana", "Rio"}];
+        let recintos =  [{bioma: 'Savana1', tamanhoEspaco:10 },
+                         {bioma: 'floresta', tamanhoEspaco: 5},
+                         {bioma: 'SavanaRio', tamanhoEspaco: 7},
+                         {bioma: 'Rio', tamanhoEspaco: 8},
+                         {bioma: 'Savana5', tamanhoEspaco: 9}];
+
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
